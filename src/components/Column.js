ea9da59e40e3fb5444fb5c7583f452f29b3d026a
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-04-06 13:50:35 -0400

    Added styled components

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Todo from './Todo';
+import styled from 'styled-components'
 
+const Container = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  padding: 5px;
+  width: 50%;
+  margin: 0 auto;
+  text-align: center;
+  background-color: tomato;
+`
+
+const Title = styled.h3`
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: white;
+  font-size: 36px;
+`
 
 export default ({col, todos}) => (
-  <div>
-    <h3>{col.title}</h3>
+  <Container>
+    <Title>{col.title}</Title>
     {todos.map(todo => (
       <Todo todo={todo}/>
     ))}
-  </div>
+  </Container>
 )
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  margin: 10px 0;
+  background-color: #fafafa;
+  padding: 10px 0;
+`
 
 export default ({todo}) => (
-  <div>{todo.content}</div>
+  <Container>{todo.content}</Container>
 )
